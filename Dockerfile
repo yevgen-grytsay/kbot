a8239a81ea34bddc7d7bbc299325f6fd4277e4a3
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-05-20 11:47:21 +0300

    otel instrumentation

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM quay.io/projectquay/golang:1.20 as builder
+FROM quay.io/projectquay/golang:1.21 as builder
 
 ARG TARGETOS
 ARG TARGETARCH
--- main.go
+++ main.go
@@ -3,8 +3,25 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/yevgen-grytsay/kbot/cmd"
+import (
+	"context"
+	"log"
+
+	"github.com/yevgen-grytsay/kbot/app"
+	"github.com/yevgen-grytsay/kbot/cmd"
+	"github.com/yevgen-grytsay/kbot/otel"
+)
 
 func main() {
+	tp, mp := otel.InitTracer(app.AppVersion)
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+		if err := mp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down metric provider: %v", err)
+		}
+	}()
+
 	cmd.Execute()
 }
